32b0fce5a351c1cf55cf8fd629db92e06ba3e595
jingshu <liujish931@hotmail.com>
2019-03-18 09:02:30 +0100

    cloud test

--- reproduce_2018.sh
+++ reproduce_2018.sh
@@ -7,7 +7,7 @@ python3 map_embeddings.py --aaai2018 data/dictionaries/en-it.train.txt data/embe
 python3 eval_translation.py data/embeddings/en.txt data/embeddings/it.txt -d data/dictionaries/en-it.test.txt
 
 ### acl2018
-python3 map_embeddings.py --acl2018 --verbose data/embeddings/original/ELMo/en.txt data/embeddings/original/ELMo/fr.txt data/embeddings/mapped/ELMo/en-fr/en.txt data/embeddings/mapped/ELMo/fen-fr/r.txt
+python3 map_embeddings.py --acl2018 --verbose data/embeddings/original/ELMo/en.txt data/embeddings/original/ELMo/fr.txt data/embeddings/mapped/ELMo/en-fr/en.txt data/embeddings/mapped/ELMo/en-fr/fr.txt
 ### acl2018 NGC
 python3 map_embeddings.py --acl2018 --verbose --cuda /home/ubuntu/data/vecamp_data/data/embeddings/original/ELMo/en.txt /home/ubuntu/data/vecamp_data/data/embeddings/original/ELMo/fr.txt /home/ubuntu/data/vecamp_data/data/embeddings/mapped/ELMo/en-fr/en.txt /home/ubuntu/data/vecamp_data/data/embeddings/mapped/ELMo/en-fr/fr.txt
 ### acl 2018 no stochastic dictionary induction
